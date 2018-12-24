2b1729b2f97ce6a16eb4e30a16f8ce96de6e5bf7
oleg7 <oleg7101112@gmail.com>
2018-12-24 19:44:56 +0200

    add PBKDF2 password hashing

--- src/main/java/ua/training/model/dao/mysql/UserMySqlDao.java
+++ src/main/java/ua/training/model/dao/mysql/UserMySqlDao.java
@@ -1,18 +1,25 @@
 package ua.training.model.dao.mysql;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.log4j.Logger;
 import ua.training.controller.MainServlet;
 import ua.training.model.dao.UserDao;
 import ua.training.model.entity.User;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.*;
 import java.util.List;
 
 public class UserMySqlDao implements UserDao {
-    private static final Logger logger = Logger.getLogger(MainServlet.class);
-    public static final String CREATE_USER = "INSERT INTO users(username, password, role, money, first_name, second_name) VALUES (?, ?, ?, ?, ?, ?)";
-    public static final String GET_LAST_ID = "SELECT id FROM users WHERE username=?;";
-    public static final String GET_BY_USERNAME_PASSWORD = "SELECT * FROM users WHERE username=? AND password=?;";
+    private static final Logger logger = Logger.getLogger(UserMySqlDao.class);
+    private static final String CREATE_USER = "INSERT INTO users(username, password, role, money, first_name, second_name) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String GET_LAST_ID = "SELECT id FROM users WHERE username=?;";
+    private static final String GET_BY_USERNAME_PASSWORD = "SELECT * FROM users WHERE username=? AND password=?;";
 
     Connection connection = ConnectionPool.getConnection();
     @Override
@@ -31,10 +38,12 @@ public class UserMySqlDao implements UserDao {
     }
 
     public User getUserByUsernameAndPassword(String username, String password) throws SQLException {
+        logger.info("Thread name in UserDAO" + Thread.currentThread().getName());
+        String hashedPassword = hashPassword(password);
         User user = new User();
         try (PreparedStatement statement = connection.prepareStatement(GET_BY_USERNAME_PASSWORD)) {
             statement.setString(1, username);
-            statement.setString(2, password);
+            statement.setString(2, hashedPassword);
             ResultSet rs = statement.executeQuery();
             if (rs.next()) {
                 user.setId(rs.getInt(1));
@@ -51,12 +60,14 @@ public class UserMySqlDao implements UserDao {
 
         return user;
     }
+
     @Override
-    public void createUser(User user) throws SQLException {
+    public void createUser(User user) {
+        String hashedPassword = hashPassword(user.getPassword());
         try {
             try (PreparedStatement statement = connection.prepareStatement(CREATE_USER)) {
                 statement.setString(1, user.getUsername());
-                statement.setString(2, user.getPassword());
+                statement.setString(2, hashedPassword);
                 statement.setString(3, user.getRole().name());
                 statement.setLong(4, user.getMoney());
                 statement.setString(5, user.getFirstName());
@@ -71,9 +82,29 @@ public class UserMySqlDao implements UserDao {
                 }
             }
 
-        } catch (Exception e) {
+        } catch (SQLIntegrityConstraintViolationException e) {
             logger.error("Not unique value", e);
             throw new UsernameIsTakenException();
+        } catch (SQLException e) {
+            logger.error("SQL exception", e);
+        }
+    }
+
+    private String hashPassword(String password) {
+        String salt = "1234";
+        int iterations = 10000;
+        int keyLength = 512;
+        char[] passwordChars = password.toCharArray();
+        byte[] saltBytes = salt.getBytes();
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            PBEKeySpec spec = new PBEKeySpec( passwordChars, saltBytes, iterations, keyLength );
+            SecretKey key = skf.generateSecret( spec );
+            byte[] res = key.getEncoded( );
+            return Hex.encodeHexString(res);
+        } catch ( NoSuchAlgorithmException | InvalidKeySpecException e ) {
+            logger.error("Hash algorithm does not work");
+            throw new RuntimeException(e);
         }
     }
 }
