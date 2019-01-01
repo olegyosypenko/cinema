5196125a8854cb4438493ada8c552e6b4a4dbdf6
oleg7 <oleg7101112@gmail.com>
2019-01-01 19:59:36 +0200

    fix problem with self destroyed session

--- src/main/java/ua/training/controller/listeners/SessionListener.java
+++ src/main/java/ua/training/controller/listeners/SessionListener.java
@@ -4,9 +4,11 @@ import org.apache.log4j.Logger;
 import ua.training.model.entity.Role;
 import ua.training.model.entity.User;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.List;
 
 public class SessionListener implements HttpSessionListener {
     private static final Logger logger = Logger.getLogger(SessionListener.class);
@@ -23,6 +25,11 @@ public class SessionListener implements HttpSessionListener {
     @Override
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
         HttpSession session = httpSessionEvent.getSession();
+        User user = (User) session.getAttribute("user");
+        ServletContext context = session.getServletContext();
+        List<User> users = (List<User>) context.getAttribute("logged-users");
+        users.remove(user);
         logger.info("Session destroyed: " + session.getId());
     }
+
 }
