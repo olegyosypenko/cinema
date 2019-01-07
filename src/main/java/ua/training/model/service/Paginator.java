440c3dc3967530c5dedb75f98b422139042149a4
oleg7 <oleg7101112@gmail.com>
2019-01-07 22:49:30 +0200

    rename method

--- src/main/java/ua/training/model/service/Paginator.java
+++ src/main/java/ua/training/model/service/Paginator.java
@@ -15,9 +15,7 @@ public class Paginator<E> {
         this.currentIndex = 1;
     }
 
-
-
-    private int getAmountOfPages() {
+    private int getNumberOfPages() {
         if (items.size() % numberOfItems != 0) {
             return items.size() / numberOfItems + 1;
         } else {
@@ -26,19 +24,27 @@ public class Paginator<E> {
 
     }
 
+    /**
+     * Creates list of indexes that can be applied
+     */
     public List<Integer> getIndexes() {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < getAmountOfPages(); i++) {
+        for (int i = 0; i < getNumberOfPages(); i++) {
             list.add(i + 1);
         }
         return list;
     }
 
+    /**
+     * This method returns part of items specified by index.
+     * and sets index to currentIndex.
+     * @return required part of list
+     */
     public List<E> getItemsPart(int index) {
         if (index < 1){
             index = 1;
-        } else if (index > getAmountOfPages()) {
-            index = getAmountOfPages();
+        } else if (index > getNumberOfPages()) {
+            index = getNumberOfPages();
         }
         this.currentIndex = index;
         int i = index - 1;
@@ -49,6 +55,9 @@ public class Paginator<E> {
         return items.subList(i * numberOfItems, i * numberOfItems + numberOfItems);
     }
 
+    /**
+     * Returns previous index or 1 if currentIndex is 1
+     */
     public int getPreviousIndex() {
         if (currentIndex == 1) {
             return currentIndex;
@@ -56,14 +65,20 @@ public class Paginator<E> {
             return currentIndex - 1;
         }
     }
+    /**
+     * Returns next index or last index if currentIndex is last index
+     */
     public int getNextIndex() {
-        if (currentIndex == getAmountOfPages()) {
+        if (currentIndex == getNumberOfPages()) {
             return currentIndex;
         } else {
             return currentIndex + 1;
         }
     }
 
+    /**
+     * Returns current index
+     */
     public int getCurrentIndex() {
         return currentIndex;
     }
