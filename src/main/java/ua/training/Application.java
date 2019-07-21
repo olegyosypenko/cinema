df5de712cc1316da4e45ef88f73b60b4f1f7c01f
Oleg Yosypenko <yosypenko.oleg@gmail.com>
2019-07-22 00:19:38 +0300

    Made compatible J2EE version of project with Spring Boot

--- src/main/java/ua/training/Application.java
+++ src/main/java/ua/training/Application.java
@@ -2,10 +2,62 @@ package ua.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import ua.training.controller.MainServlet;
+import ua.training.controller.filters.AuthFilter;
+import ua.training.controller.filters.EncodingFilter;
+import ua.training.controller.filters.LocalizationFilter;
+import ua.training.controller.listeners.ContextListener;
+import ua.training.controller.listeners.SessionListener;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new MainServlet(), "/servlet/*");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean encodingFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new EncodingFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean localizationFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new LocalizationFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean authFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new AuthFilter());
+        filterRegistrationBean.setOrder(3);
+        filterRegistrationBean.addUrlPatterns("/servlet/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean contextListener() {
+        ServletListenerRegistrationBean listenerReg = new ServletListenerRegistrationBean(new ContextListener());
+        return listenerReg;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean sessionListener() {
+        ServletListenerRegistrationBean listenerReg = new ServletListenerRegistrationBean(new SessionListener());
+        return listenerReg;
     }
 }
