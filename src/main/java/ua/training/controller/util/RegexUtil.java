5dc57670bc9162a4288e6924136265739d9da90a
Oleh Yosypenko <Oleh_Yosypenko@epam.com>
2020-01-07 17:58:41 +0200

    [BE] Created RegexUtil class

---
+++ src/main/java/ua/training/controller/util/RegexUtil.java
@@ -0,0 +1,26 @@
+package ua.training.controller.util;
+
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexUtil {
+    private static final String BUNDLE_NAME = "regex";
+    private static final ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    /**
+     * Method is retrieves regex by regexCode and checks if input matches the regex.
+     *
+     * @param regexCode code of regex
+     * @param input input to be checked
+     * @return result of check
+     */
+    public static boolean matches(String regexCode, String input) {
+        if (regexCode == null || input == null) {
+            return false;
+        }
+        String regex = bundle.getString(regexCode);
+        Matcher matcher = Pattern.compile(regex).matcher(input);
+        return matcher.matches();
+    }
+}
