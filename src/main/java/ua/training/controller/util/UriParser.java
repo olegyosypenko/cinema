19a0f79cb2ce9eb5e6fe2388ca1c752925f5ce32
Oleh Yosypenko <Oleh_Yosypenko@epam.com>
2020-01-15 23:34:22 +0200

    [BE][REFACTORING] Refactored UriParser and added tests

--- src/main/java/ua/training/controller/util/UriParser.java
+++ src/main/java/ua/training/controller/util/UriParser.java
@@ -5,22 +5,19 @@ import org.apache.log4j.Logger;
 
 public class UriParser {
     private static Logger logger = Logger.getLogger(UriParser.class);
+
     public static int getIndexFromUri(String uri) {
         String[] uriParts = uri.split("/");
-        String indexPart = uri.split("/")[uriParts.length - 1];
+        String indexPart = uriParts[uriParts.length - 1];
         logger.info("index: " + indexPart);
         return Integer.parseInt(indexPart);
     }
 
     public static String getCommandNameFromUri(String uri) {
-        String result = uri.replaceFirst(".*/servlet/", "");
-        result = result.replaceFirst("/[0-9-]+", "");
-        return result;
+        return uri.replaceAll(".*/servlet/([\\w/]*)/[0-9-]+", "$1");
     }
 
     public static String getAccessLabel(String uri) {
-        String result = uri.replaceFirst(".*/servlet/", "");
-        result = result.replaceAll("/.*", "");
-        return result;
+        return uri.replaceAll(".*/servlet/(\\w*)/.*", "$1");
     }
 }
---
+++ src/test/java/ua/training/controller/util/UriParserTest.java
@@ -0,0 +1,37 @@
+package ua.training.controller.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UriParserTest {
+
+    @Test
+    public void shouldReturnIndex() {
+        int expected = 2;
+
+        int actual = UriParser.getIndexFromUri("test/uri/index/2");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCommandName() {
+        String expected = "user/command";
+        String uri = "cinema/servlet/user/command/32";
+
+        String actual = UriParser.getCommandNameFromUri(uri);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnAccessLabel() {
+        String expected = "user";
+        String uri = "cinema/servlet/user/command/32";
+
+        String actual = UriParser.getAccessLabel(uri);
+
+        assertEquals(expected, actual);
+    }
+}
